70c78e36e4566e7a0c1d2fc2f803487569780082
RabieImghi <rabieaitimghi7@gmail.com>
2024-12-23 20:08:56 +0100

    create header and hero componenets

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  isMenuOpen = false;
+  activeLink = 'home';
+  isScrolled = false;
+
+  constructor(private router: Router) {}
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    this.isScrolled = scrollTop > 50;
+  }
+  
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  setActiveLink(link: string) {
+    this.activeLink = link;
+  }
+}
