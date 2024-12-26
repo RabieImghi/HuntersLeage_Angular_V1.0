d34de56670fb0be7fc39a2eb521d22ceaece63e6
RabieImghi <rabieaitimghi7@gmail.com>
2024-12-26 17:26:22 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { SpeceisComponent } from './componenet/speceis/speceis.component';
 import { LoginComponent } from './componenet/auth/login/login.component';
 import { RegisterComponent } from './componenet/auth/register/register.component';
 import { TopThreeComponent } from './componenet/top-three/top-three.component';
+import { ResultComponent } from './componenet/result/result.component';
 export const routes: Routes = [
     {path: '', component: HeroComponent},
     {path: 'competitions', component: CompitionsComponent},
     {path: 'species', component: SpeceisComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
-    {path: 'top-three-user', component: TopThreeComponent}
+    {path: 'top-three-user', component: TopThreeComponent},
+    {path: 'results', component: ResultComponent}
 ];
---
+++ src/app/componenet/result/result.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-result',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './result.component.html'
+})
+export class ResultComponent {
+  userHistoricResponse = {
+    user: {
+      name: 'Ava M. Charlotte',
+      avatar: '/placeholder.svg?height=32&width=32',
+    },
+    pastCompetitions: [
+      {
+        id: '123e4567-e89b-12d3-a456-426614174000',
+        code: 'CMP-001',
+        speciesType: 'Mammals',
+        rank: 1,
+        score: 98.5,
+      },
+      {
+        id: '123e4567-e89b-12d3-a456-426614174001',
+        code: 'CMP-002',
+        speciesType: 'Birds',
+        rank: 3,
+        score: 75.3,
+      },
+      {
+        id: '123e4567-e89b-12d3-a456-426614174002',
+        code: 'CMP-003',
+        speciesType: 'Reptiles',
+        rank: 2,
+        score: 88.2,
+      },
+    ],
+  };
+}
