505176bceef1eb682ffbb2c6cac188fbaaa17059
RabieImghi <rabieaitimghi7@gmail.com>
2024-12-28 14:49:40 +0100

    add delete compition modal

--- src/app/front-admin/CRUD/hunts/delete-hunt/delete-hunt.component.ts
+++ src/app/front-admin/CRUD/hunts/delete-hunt/delete-hunt.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-delete-hunt',
@@ -11,4 +12,23 @@ export class DeleteHuntComponent {
 
   @Input() hunt: any;
 
+  
+    deleteHunt() {
+      Swal.fire({
+        title: 'Are you sure?',
+        text: `Do you really want to delete hunt with ID: ${this.hunt.id}?`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'Cancel',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire('Deleted!', 'The hunt has been deleted.', 'success');
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          Swal.fire('Cancelled', 'The hunt is safe :)', 'info');
+        }
+      });
+    }
 }
