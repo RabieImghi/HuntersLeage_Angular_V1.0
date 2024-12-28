f66a9edb45ff1c5f986273850716ea0b168e2ef8
RabieImghi <rabieaitimghi7@gmail.com>
2024-12-28 12:38:51 +0100

    add update compitions  component admin template

--- src/app/front-admin/CRUD/update-competitions/update-competitions.component.ts
+++ src/app/front-admin/CRUD/update-competitions/update-competitions.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-update-competitions',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './update-competitions.component.html',
-  styleUrl: './update-competitions.component.css'
+  animations: [
+    trigger('fadeAnimation', [
+      transition(':enter', [style({ opacity: 0 }), animate('300ms ease-out', style({ opacity: 1 }))]),
+      transition(':leave', [animate('300ms ease-in', style({ opacity: 0 }))]),
+    ]),
+    trigger('scaleAnimation', [
+      transition(':enter', [
+        style({ transform: 'scale(0)', opacity: 0 }),
+        animate('300ms ease-out', style({ transform: 'scale(1)', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ transform: 'scale(0)', opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class UpdateCompetitionsComponent {
 
+  @Input() competition: any;
+
+  competitionTypes = [
+    { value: 'SEA', label: 'SEA (9)' },
+    { value: 'BIG_GAME', label: 'BIG GAME (3)' },
+    { value: 'BIRD', label: 'BIRD (5)' },
+  ];
+  isModalOpenUpdate = false;
+
+  toggleModalUpdate(state: boolean): void {
+    this.isModalOpenUpdate = state;
+  }
 }
--- src/app/front-admin/manage-competitions/manage-competitions.component.ts
+++ src/app/front-admin/manage-competitions/manage-competitions.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateCompetitionsComponent } from '../CRUD/create-competitions/create-competitions.component';
 import { DeleteCompetitionsComponent } from '../CRUD/delete-competitions/delete-competitions.component';
+import { UpdateCompetitionsComponent } from '../CRUD/update-competitions/update-competitions.component';
 
 interface CompetitionResponseVm {
   id: string;
@@ -17,7 +18,7 @@ interface CompetitionResponseVm {
 @Component({
   selector: 'app-manage-competitions',
   standalone: true,
-  imports: [CommonModule, CreateCompetitionsComponent, DeleteCompetitionsComponent],  
+  imports: [CommonModule, CreateCompetitionsComponent, DeleteCompetitionsComponent, UpdateCompetitionsComponent],  
   templateUrl: './manage-competitions.component.html'
 })
 
@@ -30,7 +31,7 @@ export class ManageCompetitionsComponent {
       code: 'COMP-001',
       location: 'New York City',
       date: '2024-03-12T08:00:00',
-      speciesType: 'Birds',
+      speciesType: 'BRID',
       minParticipants: 10,
       maxParticipants: 50,
       openRegistration: true,
@@ -40,10 +41,20 @@ export class ManageCompetitionsComponent {
       code: 'COMP-002',
       location: 'Los Angeles',
       date: '2024-03-14T09:00:00',
-      speciesType: 'Fish',
+      speciesType: 'BIG_GAME',
       minParticipants: 5,
       maxParticipants: 20,
       openRegistration: false,
+    },
+    {
+      id: '123e4567-e89b-12d3-a451-456614174001',
+      code: 'COMP-003',
+      location: 'Morrocco',
+      date: '2024-03-14T09:00:00',
+      speciesType: 'SEA',
+      minParticipants: 10,
+      maxParticipants: 20,
+      openRegistration: true,
     },
   ];
 
