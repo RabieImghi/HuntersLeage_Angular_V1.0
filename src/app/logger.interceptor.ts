401c3635543d3299dab29cc9342ca7cbd39a0066
RabieImghi <rabieaitimghi7@gmail.com>
2025-01-07 21:20:45 +0100

    version final

--- src/app/logger.interceptor.ts
+++ src/app/logger.interceptor.ts
@@ -1,14 +1,24 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { TokenStorageServiceService } from './service/token-storage-service.service';
+import { Router } from '@angular/router';
 
 export const loggerInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = new Router();
   const token = localStorage.getItem('auth-token');
+  const tokenStorage = new TokenStorageServiceService();
   if (token) {
-    const authReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`),
-    });
-    return next(authReq); 
+    if(tokenStorage.isTokenExpired()){
+      localStorage.removeItem('auth-token');
+      router.navigate(['/login']);
+      return next(req);
+    }else{
+      const authReq = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`),
+      });
+      return next(authReq);
+    }
+     
   }else return next(req);
-  //   return next(req);
  
   
 };
